a90da4ba3753bbc3be984a3599fc70c78db3f012
Tatrakrad <riddick.taylor@gmail.com>
2016-11-16 13:34:34 -0600

    checks for a valid year with regex

--- day-13-coolsite/async-baseball-exercise.js
+++ day-13-coolsite/async-baseball-exercise.js
@@ -1,4 +1,21 @@
 //read and log csv
+// var fs = require('fs');
+//   var babeRuth = '';
+//
+//
+// var output = fs.readFile('players_r_ruthba01_batting_standard.csv', 'utf-8',function(err,data){
+//   var lines = data.split('\n');
+//   var year = lines[1].split(',').indexOf('Year');
+//   var rbi = lines[1].split(',').indexOf('RBI');
+//
+//   for (var i=0;i<lines.length;i++){
+//     var rowArr = lines[i].split(',');
+//     babeRuth = rowArr[year] + " " + rowArr[rbi] + "\n";
+//     console.log(babeRuth);
+//
+//   }
+// });
+// read and log csv
 var fs = require('fs');
   var babeRuth = '';
 
@@ -7,10 +24,17 @@ var output = fs.readFile('players_r_ruthba01_batting_standard.csv', 'utf-8',func
   var lines = data.split('\n');
   var year = lines[1].split(',').indexOf('Year');
   var rbi = lines[1].split(',').indexOf('RBI');
+  var isYear = /\d\d\d\d/g;
 
-  for (var i=0;i<lines.length;i++){
+  for (var i=1;i<lines.length;i++){
     var rowArr = lines[i].split(',');
-    babeRuth = rowArr[year] + " " + rowArr[rbi] + "\n";
-    console.log(babeRuth);
+    //console.log(rowArr[year].match(isYear), rowArr[year]);
+    if(rowArr[year].match(isYear)){
+      babeRuth = rowArr[year] + " " + rowArr[rbi] + "\n";
+      console.log(babeRuth);
+    }else{
+      continue;
+    }
+
   }
 });
