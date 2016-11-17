c93093b2fe09489cd00520716005f17f49ab034c
Tatrakrad <riddick.taylor@gmail.com>
2016-11-17 16:55:59 -0600

    office 365 assignment working poorly

--- day-13-office365/officeparse.js
+++ day-13-office365/officeparse.js
@@ -2,22 +2,25 @@
 var fs = require('fs');
 
 var fileContents = fs.readFile('./Import_User_Sample_en.csv','utf-8',function(err,officeData){
-  console.log('Gutting some CSV');
+  //console.log('cutting up some CSV');
   console.log('err', err);
   console.log('data', officeData);
 
   var rows = officeData.split('\n');
-  var userName = rows[0].indexOf('User Name');
-  var displayName = rows[0].indexOf('Display Name');
+  console.log(rows[0]);
+  // var userName = rows[0].split(',').indexOf('User Name');
+  var userName = 0;
+  console.log("User name is column..." + userName);
+  var displayName = rows[0].split(',').indexOf('Display Name');
+  console.log("displayName is column..." + displayName);
 
-  for (var i = 1;i<rows.length;i++){
-    var officeColumn = rows[i];
-    officeColumn = officeColumn.split(",");
-    console.log(officeColumn[userName],officeColumn[displayName]);
+  for (var i = 0;i<rows.length;i++){
+    var rowArray = rows[i].split(",");
+    console.log(rowArray[userName],rowArray[displayName]);
 
   }
 
-
+  console.log("i'm done");
 });
 
 console.log('Doing Things');
