a14680736189912ad6f81be02822f25c97347201
Tatrakrad <riddick.taylor@gmail.com>
2016-11-29 17:08:14 -0600

    toggling improvements. padding replaces text indent

--- day-22-gmail-menu-toggle/script.js
+++ day-22-gmail-menu-toggle/script.js
@@ -3,7 +3,15 @@ var categories = document.querySelector('.categories');
 var catSelect = function(event){
   console.log(event.target);
   if(event.target.tagName === 'LI'){
+    //li detected, remove all other selected status from list-style
+    var deselect = document.querySelectorAll('li');
+    for(var i = 0; i<deselect.length; i++){
+      if(event.target !== deselect[i]){
+        deselect[i].classList.remove('selected');
+      }
+    }
     event.target.classList.toggle('selected');
+
   }
 } ;
 
