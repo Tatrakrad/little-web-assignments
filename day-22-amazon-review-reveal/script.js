16f518207925c8048d3d40117509e2cefb1552fa
Tatrakrad <riddick.taylor@gmail.com>
2016-11-30 16:18:05 -0600

    animation works with height in pixels

--- day-22-amazon-review-reveal/script.js
+++ day-22-amazon-review-reveal/script.js
@@ -1,3 +1,5 @@
+// find height of first child of article, set height of article to that
+//find height of last child, set post transition height there?
 (function(){
 
 var reviewText = document.querySelector('.review');
@@ -9,6 +11,7 @@ readMore.addEventListener('click',function(){
   reviewText.classList.toggle('reveal');
   if (readLess){
     readmore.innerHTML="<a>Read More<a>";
+    readLess = false;
   }else{
     readmore.innerHTML="<a>Read Less</a>";
     readLess = true;
