219e8a3073c538b8333f5c2092e89033fa94fec8
Tatrakrad <riddick.taylor@gmail.com>
2016-12-04 19:36:44 -0600

    no sprite slicing in js

--- day-24-useless/script.js
+++ day-24-useless/script.js
@@ -25,20 +25,7 @@
   };
   //spritesheet @600% splits 64x64
   //svg-sprite-social has 5 rows 6 columns
-  var populatePositions = function(size,rows,columns){
-    var sliceNum = rows * columns;
-    var xpos = 0;
-    var ypos = 0;
-    for(var i=0; i<=sliceNum; i++){
-      for (var j=0; i<=rows; i++){
-          ypos -= 64;
-        for (var k=0; i<=columns; i++){
-          xpos -=64;
-        }
-      }
-    }
 
-  };
 
 
 }());
