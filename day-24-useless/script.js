bb4605715b5d013c651d320a6e7d3531b9b966f0
Tatrakrad <riddick.taylor@gmail.com>
2016-12-05 11:48:29 -0600

    generates random outcomes very slowly and i did not remove old classes when changing values plus the reset does not work

--- day-24-useless/script.js
+++ day-24-useless/script.js
@@ -11,23 +11,51 @@
   //svg-sprite-social has 5 rows 6 columns, 30 potential outcomes per reel
 
 
-
-
   //slot machine object
-  var slotMachineReels = {
+  var slotMachine = {
     spinning:false,
     reels:[document.getElementById('reel-one'),document.getElementById('reel-two'),document.getElementById('reel-three'),document.getElementById('reel-four')],
-    reelPositions:[18,18,18,18],
+    reelPositions:[30,30,30,30],
     setReelPos: function(pos1,pos2,pos3,pos4){
       var newPos1 = potentialPositions[pos1];
       var newPos2 = potentialPositions[pos2];
       var newPos3 = potentialPositions[pos3];
       var newPos4 = potentialPositions[pos4];
-      console.log(this.reels[0].classlist);
+      console.log(slotMachine.reels[0]);
       this.reels[0].classList.toggle(newPos1);
       this.reels[1].classList.toggle(newPos2);
       this.reels[2].classList.toggle(newPos3);
       this.reels[3].classList.toggle(newPos4);
+    },
+    spin: function(){
+      var newPos = [0,0,0,0];
+      var spinTime = setInterval(function(){
+        spinning=true;
+        for(var i=0; i<=4;i++){
+          var random = Math.ceil(Math.random()*potentialPositions.length);
+          newPos[i]=random;
+          // console.log(newPos);
+        }
+        slotMachine.setReelPos(newPos[0],newPos[1],newPos[2],newPos[3]);
+      },100);
+      setTimeout(function(){clearInterval(spinTime);
+        spinning = false;
+        ready = true;
+        console.log(spinning);
+        console.log(ready);
+        slotMachine.resetReels(spinning);
+        },6000);
+
+
+    },
+    resetReels: function(cantReset){
+      if(!cantReset){
+        for(var i=0;i<slotMachine.reels.length;i++){
+          slotMachine.reels[i].classname = "reel selection-box";
+        }
+      }else{
+        console.log('problems');
+      }
     }
   };
 
@@ -36,32 +64,17 @@
   var potentialPositions = ["blanktwin","cake","skyline","bell","person","smile","twoperson","addpeople","nobell","happy","addperson","dead","sad","darkbell","ringbell","sleepbell","emptyperson","laugh","selection-box","camera","doubleperson","buildings","plusone","crank","bars","earth","mortarboard","frown","neutral","fire"];
 
 
-  var spin = function(){
-    var newPos = [0,0,0,0];
-    var spinTime = setInterval(function(){
-      for(var i=0; i<slotMachineReels.reelPositions.length;i++){
-        var random = Math.ceil(Math.random()*potentialPositions.length);
-        newPos[i]=random;
-        console.log(newPos);
-      }
-      slotMachineReels.setReelPos(newPos[0],newPos[1],newPos[2],newPos[3]);
-    },300);
-    setTimeout(function(){clearInterval(spinTime);},6000);
-    ready = true;
-  };
-
-
   var play = function(){
     if (ready){
       ready = !ready;
-      spin();
+      slotMachine.spin();
 
     }else{
       console.log('not ready');
       return;
     }
   };
-
+  console.log(slotMachine.reelPositions);
   playButton.addEventListener('click',play);
 
 }());
