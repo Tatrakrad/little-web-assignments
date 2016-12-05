2b5c6ba4b5aeae4df662baacf68686523b781495
Tatrakrad <riddick.taylor@gmail.com>
2016-12-04 22:47:45 -0600

    output reels now scramble, but need to be unset to a default after a play

--- day-24-useless/script.js
+++ day-24-useless/script.js
@@ -20,13 +20,18 @@
     reelPositions:[18,18,18,18],
     setReelPos: function(pos1,pos2,pos3,pos4){
       var newPos1 = potentialPositions[pos1];
+      var newPos2 = potentialPositions[pos2];
+      var newPos3 = potentialPositions[pos3];
+      var newPos4 = potentialPositions[pos4];
       console.log(this.reels[0].classlist);
-      this.reels[0].classlist.toggle(newpos1);
+      this.reels[0].classList.toggle(newPos1);
+      this.reels[1].classList.toggle(newPos2);
+      this.reels[2].classList.toggle(newPos3);
+      this.reels[3].classList.toggle(newPos4);
     }
   };
 
-  console.log(slotMachineReels);
-  console.log(slotMachineReels.reels[0]);
+
 
   var potentialPositions = ["blanktwin","cake","skyline","bell","person","smile","twoperson","addpeople","nobell","happy","addperson","dead","sad","darkbell","ringbell","sleepbell","emptyperson","laugh","selection-box","camera","doubleperson","buildings","plusone","crank","bars","earth","mortarboard","frown","neutral","fire"];
 
@@ -40,9 +45,9 @@
         console.log(newPos);
       }
       slotMachineReels.setReelPos(newPos[0],newPos[1],newPos[2],newPos[3]);
-    },500);
-    setTimeout(function(){clearInterval(spinTime);},5000);
-
+    },300);
+    setTimeout(function(){clearInterval(spinTime);},6000);
+    ready = true;
   };
 
 
