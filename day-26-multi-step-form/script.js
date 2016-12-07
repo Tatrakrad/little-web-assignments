01a93236728ffd006076eeb7078b001e22b0a1b6
Tatrakrad <riddick.taylor@gmail.com>
2016-12-07 09:11:26 -0600

    works now, in theory, by saving last position seperately

--- day-26-multi-step-form/script.js
+++ day-26-multi-step-form/script.js
@@ -9,15 +9,23 @@ var submitButton = document.getElementById('submit');
 var formPages = document.querySelectorAll('.hidden');
 var maxPosition = formPages.length - 1;
 
+var localStorageValue = localStorage.getItem('saved-page');
+var localLastPageValue = localStorage.getItem('saved-last');
+console.log('saved page => ', localStorageValue);
 var position = 0;
 var lastPos = 0;
+
+if(localStorageValue !==null){
+  position = Number(localStorageValue);
+  lastPos = Number(localLastPageValue);
+}
 
 console.dir(formPages);
 
 console.log("num pages => ",maxPosition);
 
 //show page 1 on load
-formPages[0].classList.toggle('hidden');
+formPages[position].classList.toggle('hidden');
 
 
 prevButton.addEventListener('click',function(){
@@ -36,6 +44,7 @@ nextButton.addEventListener('click',function(){
 
 
 function switchPage(){
+  localStorage.setItem('saved-page',position);
 
   var selectOld = formPages[lastPos];
   var selectNew = formPages[position];
@@ -43,6 +52,7 @@ function switchPage(){
   selectNew.classList.toggle('hidden');
 
   lastPos = position;
+  localStorage.setItem('saved-last',lastPos);
 }
 
 
