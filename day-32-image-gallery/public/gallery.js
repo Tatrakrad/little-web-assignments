dbf859f00f66e939a29ea3650719eb7957c9c813
Tatrakrad <riddick.taylor@gmail.com>
2016-12-13 16:35:26 -0600

    basic error checking for misclicks

--- day-32-image-gallery/public/gallery.js
+++ day-32-image-gallery/public/gallery.js
@@ -34,34 +34,40 @@
     var attr;
     if (evt.target.tagName === 'LI'){
       attr = evt.target.getAttribute('pic-index');
-    }else{
+    }else if(evt.target.tagName === 'IMG'){
       attr = evt.target.parentNode.getAttribute('pic-index');
+    }else{
+
     }
 
     console.log(attr);
+    if(attr !== undefined){
 
-    var currentlySelected = document.querySelector('.selected');
-    if (currentlySelected !== null){
-      currentlySelected.classList.remove('selected');
+      var currentlySelected = document.querySelector('.selected');
+      if (currentlySelected !== null){
+        currentlySelected.classList.remove('selected');
+      }
+      evt.target.classList.add('selected');
+
+      var showPic = apiData.pictures[attr];
+      console.log("show this picture", showPic);
+      //picture object to display fullSize with description
+
+      var picDisplay = document.querySelector('.display');
+      picDisplay.innerHTML = " ";
+
+      var img = document.createElement('img');
+      img.src = showPic.fullSize;
+
+      var caption = document.createElement('h3');
+      caption.textContent = showPic.description;
+
+      //append image to display
+      picDisplay.appendChild(img);
+      picDisplay.appendChild(caption);
+
+
     }
-    evt.target.classList.add('selected');
-
-    var showPic = apiData.pictures[attr];
-    console.log("show this picture", showPic);
-    //picture object to display fullSize with description
-
-    var picDisplay = document.querySelector('.display');
-    picDisplay.innerHTML = " ";
-
-    var img = document.createElement('img');
-    img.src = showPic.fullSize;
-
-    var caption = document.createElement('h3');
-    caption.textContent = showPic.description;
-
-    //append image to display
-    picDisplay.appendChild(img);
-    picDisplay.appendChild(caption);
 
 
   });
