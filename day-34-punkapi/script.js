4be8ebd50c8ed91290d705a00c9ae002cff651f4
Tatrakrad <riddick.taylor@gmail.com>
2016-12-15 11:39:37 -0600

    punk api with paging

---
+++ day-34-punkapi/script.js
@@ -0,0 +1,48 @@
+(function() {
+
+  var nextButton = document.querySelector('.next-button');
+  nextButton.addEventListener('click',function(){
+    console.log('clicked ',nextButton);
+    page ++;
+    requestData();
+  });
+
+  //base 64 encoding/decoding for string function  (conversion for ASCII to binary for network friendly traffic);
+  var auth = btoa("5d205e6e6d7944a89e11e7b87129911c");
+  console.log(auth);
+
+  var page = 1;
+
+  function requestData(){
+
+    var promise =$.ajax({
+      type: 'GET',
+      url: "https://punkapi.com/api/v1/beers?page=" + page,
+      dataType: 'json',
+       async: true,
+       headers: {
+        "Authorization": "Basic " + auth
+      },
+    });
+
+    promise.done(function(data){
+      console.log(data);
+      // document.pre(JSON.stringify(data));
+
+      var beerStuffList = document.querySelector('.beer-stuff');
+      var templateScript = document.querySelector('.beer-template');
+      var templateHtml = templateScript.innerHTML;
+
+      var html = '';
+      for (var i = 0; i < data.length; i++){
+        var beer = data[i];
+
+        var output = Mustache.render(templateHtml, beer);
+        html += output;
+      }
+      beerStuffList.innerHTML = html;
+    });
+  }
+  requestData();
+
+}());
