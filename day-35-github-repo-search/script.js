55bdcc4218ebf5fe2e6807f342e1811177e01e98
Jordan Riddick <riddick.taylor@gmail.com>
2016-12-18 17:44:09 -0600

    basic pagination added

--- day-35-github-repo-search/script.js
+++ day-35-github-repo-search/script.js
@@ -6,6 +6,13 @@
     var searchRepos = $( '.repo-input' ).val();
     var searchButton = $('.search-button');
 
+    //paginate
+    var backButton = $('.back');
+    var nextButton = $('.next');
+    var pageDisplay = $('.page-num');
+    var pageNum = 1;
+    var maxPage;
+
     //select outputs
     var resultsDiv = $( '.results' );
     var reposList = $( ".repos-list" );
@@ -19,7 +26,7 @@
       evt.preventDefault();
       console.log(searchRepos);
       if(searchRepos.length > 0){
-        searchGithubRepos(searchRepos);
+        searchGithubRepos(searchRepos,pageNum);
       }
 
     });
@@ -27,10 +34,33 @@
     searchForm.keyup(function(evt){
       searchRepos = $( '.repo-input' ).val();
       if (evt.which === 13 && searchRepos.length > 0){
-        searchGithubRepos(searchRepos);
+        searchGithubRepos(searchRepos,pageNum);
       }
     });
 
+    nextButton.click(function(){
+      // searchRepos = $( '.repo-input' ).val();
+      if (pageNum < maxPage/30){
+        //active button
+        pageNum++;
+        searchGithubRepos(searchRepos,pageNum);
+      }else{
+        //deactivate
+      }
+
+    });
+
+    backButton.click(function(evt){
+      // searchRepos = $( '.repo-input' ).val();
+      if (pageNum > 1){
+        //activate
+        pageNum--;
+        searchGithubRepos(searchRepos,pageNum);
+      }else{
+        return;
+      }
+
+    });
 
 
     //api Call
@@ -38,7 +68,7 @@
     //api.github.com/search/users
     //api.github.com/search/repos
 
-    function searchGithubRepos(reponame){
+    function searchGithubRepos(reponame,pageNum){
 
       console.log(reponame);
       console.log("clearing output");
@@ -47,16 +77,20 @@
       var promise = $.ajax({
         type: 'GET',
         url: "https://api.github.com/search/repositories",
-        data:{q:reponame},
+        data:{q:reponame, page:pageNum},
         dataType: 'JSON',
         async:true,
       });
 
       promise.done(function(data){
-        console.log("recieved data, =||= ::",data);
+        maxPage = data.total_count;
+
         //test
+        console.log("recieved data, =||= ::",data);
+        console.log("maxpage ",maxPage)
         // resultsDiv.append(JSON.stringify(data));
 
+        pageDisplay.html(pageNum);
         renderPage(data);
 
       });
