a7907806975b2c93c362646ef284ac45890eeb93
Tatrakrad <riddick.taylor@gmail.com>
2017-01-06 15:25:41 -0600

    basic github repo search working in react

--- day-44-github-repo-search-react/client/js/script.js
+++ day-44-github-repo-search-react/client/js/script.js
@@ -1,6 +1,11 @@
 (function() {
 
   var mountNode = document.querySelector('#react-root');
+  var auth = btoa("4d892accbfd6b61cb0118fe2a5ce0baeebe5dad7");
+
+  //placeholder
+  var pageNum=1;
+  var query="blue boomerang";
 
   class InterfaceComponent extends React.Component {
 
@@ -8,9 +13,30 @@
       super();
     }
 
+    keyUpHappened(evt){
+      console.log(evt.keyCode);
+      if (evt.keyCode === 13){
+        console.log(evt.target.value);
+        // GithubRepos.searchRepos(evt.target.value);
+        this.setState({
+          request:evt.target.value
+        })
+      }
+    }
 
     render(){
-      return (<div>    </div>);
+      var doSearch;
+
+      if (this.state !== null){
+        doSearch = <GithubRepos query={this.state.request}/>;
+      }
+
+      return (<div className="search-box">
+        <input className = "repo-input"
+        placeholder="Type a repo name and hit Enter"
+        onKeyUp={(evt) => {this.keyUpHappened(evt)}}/>
+        {doSearch}
+      </div>);
     }
 
   }
@@ -20,22 +46,63 @@
     constructor(){
       super();
 
-
     }
 
-    searchGithubRepos(){
+    componentDidMount(){
+      console.log('component mounted');
+      this.searchRepos(this.props.query);
+    }
 
+    searchRepos(query){
+      $.ajax({
+        type: 'GET',
+        url: "https://api.github.com/search/repositories",
+        data:{q:query, page:pageNum},
+        dataType: 'JSON',
+        async:true,
+        headers:{
+          "Authorization": "Basic " + auth
+        },
+      })
+      .done((repoData) => {
+        //test
+        console.log("recieved data ==>", repoData);
+        //
+        this.setState({
+          apiResult:repoData
+        })
+      })
+      .fail(() => {
+        console.log("API Call Failure");
+        window.alert('API FAILURE');
+      });
 
     }
 
     render(){
-      return <div className="repos-list hidden"> Results </div>
+      var searchResults;
+
+      if (this.state != null){
+        searchResults =
+         <ul>
+          {this.state.apiResult.items.map((repo) => {return (
+            <li key={repo.id}>
+              <div>{repo.name}</div>
+              <div>{repo.owner.login}</div>
+            </li>
+          )})}
+        </ul>;
+      }
+
+      return <div className="repos-list"> {searchResults} </div>
     }
   }
 
   class AppComponent extends React.Component {
     render() {
-      return <div>React works!</div>;
+      return (<div>React works!
+        <InterfaceComponent />
+      </div>);
     }
   }
 
--- day-44-github-repo-search-react/public/script.js
+++ day-44-github-repo-search-react/public/script.js
@@ -8,53 +8,169 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var mountNode = document.querySelector('#react-root');
+(function () {
 
-var GithubRepos = function (_React$Component) {
-  _inherits(GithubRepos, _React$Component);
+  var mountNode = document.querySelector('#react-root');
+  var auth = btoa("4d892accbfd6b61cb0118fe2a5ce0baeebe5dad7");
 
-  function GithubRepos() {
-    _classCallCheck(this, GithubRepos);
+  //placeholder
+  var pageNum = 1;
+  var query = "blue boomerang";
 
-    return _possibleConstructorReturn(this, (GithubRepos.__proto__ || Object.getPrototypeOf(GithubRepos)).apply(this, arguments));
-  }
+  var InterfaceComponent = function (_React$Component) {
+    _inherits(InterfaceComponent, _React$Component);
 
-  _createClass(GithubRepos, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        { className: "repos-list hidden" },
-        " Results "
-      );
+    function InterfaceComponent() {
+      _classCallCheck(this, InterfaceComponent);
+
+      return _possibleConstructorReturn(this, (InterfaceComponent.__proto__ || Object.getPrototypeOf(InterfaceComponent)).call(this));
     }
-  }]);
 
-  return GithubRepos;
-}(React.Component);
+    _createClass(InterfaceComponent, [{
+      key: "keyUpHappened",
+      value: function keyUpHappened(evt) {
+        console.log(evt.keyCode);
+        if (evt.keyCode === 13) {
+          console.log(evt.target.value);
+          // GithubRepos.searchRepos(evt.target.value);
+          this.setState({
+            request: evt.target.value
+          });
+        }
+      }
+    }, {
+      key: "render",
+      value: function render() {
+        var _this2 = this;
 
-var AppComponent = function (_React$Component2) {
-  _inherits(AppComponent, _React$Component2);
+        var doSearch;
 
-  function AppComponent() {
-    _classCallCheck(this, AppComponent);
+        if (this.state !== null) {
+          doSearch = React.createElement(GithubRepos, { query: this.state.request });
+        }
 
-    return _possibleConstructorReturn(this, (AppComponent.__proto__ || Object.getPrototypeOf(AppComponent)).apply(this, arguments));
-  }
+        return React.createElement(
+          "div",
+          { className: "search-box" },
+          React.createElement("input", { className: "repo-input",
+            placeholder: "Type a repo name and hit Enter",
+            onKeyUp: function onKeyUp(evt) {
+              _this2.keyUpHappened(evt);
+            } }),
+          doSearch
+        );
+      }
+    }]);
 
-  _createClass(AppComponent, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        "React works!"
-      );
+    return InterfaceComponent;
+  }(React.Component);
+
+  var GithubRepos = function (_React$Component2) {
+    _inherits(GithubRepos, _React$Component2);
+
+    function GithubRepos() {
+      _classCallCheck(this, GithubRepos);
+
+      return _possibleConstructorReturn(this, (GithubRepos.__proto__ || Object.getPrototypeOf(GithubRepos)).call(this));
     }
-  }]);
 
-  return AppComponent;
-}(React.Component);
+    _createClass(GithubRepos, [{
+      key: "componentDidMount",
+      value: function componentDidMount() {
+        console.log('component mounted');
+        this.searchRepos(this.props.query);
+      }
+    }, {
+      key: "searchRepos",
+      value: function searchRepos(query) {
+        var _this4 = this;
 
-ReactDOM.render(React.createElement(AppComponent, null), mountNode);
+        $.ajax({
+          type: 'GET',
+          url: "https://api.github.com/search/repositories",
+          data: { q: query, page: pageNum },
+          dataType: 'JSON',
+          async: true,
+          headers: {
+            "Authorization": "Basic " + auth
+          }
+        }).done(function (repoData) {
+          //test
+          console.log("recieved data ==>", repoData);
+          //
+          _this4.setState({
+            apiResult: repoData
+          });
+        }).fail(function () {
+          console.log("API Call Failure");
+          window.alert('API FAILURE');
+        });
+      }
+    }, {
+      key: "render",
+      value: function render() {
+        var searchResults;
+
+        if (this.state != null) {
+          searchResults = React.createElement(
+            "ul",
+            null,
+            this.state.apiResult.items.map(function (repo) {
+              return React.createElement(
+                "li",
+                { key: repo.id },
+                React.createElement(
+                  "div",
+                  null,
+                  repo.name
+                ),
+                React.createElement(
+                  "div",
+                  null,
+                  repo.owner.login
+                )
+              );
+            })
+          );
+        }
+
+        return React.createElement(
+          "div",
+          { className: "repos-list" },
+          " ",
+          searchResults,
+          " "
+        );
+      }
+    }]);
+
+    return GithubRepos;
+  }(React.Component);
+
+  var AppComponent = function (_React$Component3) {
+    _inherits(AppComponent, _React$Component3);
+
+    function AppComponent() {
+      _classCallCheck(this, AppComponent);
+
+      return _possibleConstructorReturn(this, (AppComponent.__proto__ || Object.getPrototypeOf(AppComponent)).apply(this, arguments));
+    }
+
+    _createClass(AppComponent, [{
+      key: "render",
+      value: function render() {
+        return React.createElement(
+          "div",
+          null,
+          "React works!",
+          React.createElement(InterfaceComponent, null)
+        );
+      }
+    }]);
+
+    return AppComponent;
+  }(React.Component);
+
+  ReactDOM.render(React.createElement(AppComponent, null), mountNode);
+})();
 //# sourceMappingURL=script.js.map
