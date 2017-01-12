5822ece434f34825742145b29d6938ddd1df9d9d
Tatrakrad <riddick.taylor@gmail.com>
2017-01-12 11:42:09 -0600

    buttons appear and navigate but dont work correctly

--- day-44-github-repo-search-react/client/js/script.js
+++ day-44-github-repo-search-react/client/js/script.js
@@ -1,22 +1,24 @@
 (function() {
 
+  //data should flow ONE WAY
+  //or seperate function to object outside components and use as argument
+
   var mountNode = document.querySelector('#react-root');
   var auth = btoa("4d892accbfd6b61cb0118fe2a5ce0baeebe5dad7");
 
   //placeholder
-  var pageNum=1;
-  var query="blue boomerang";
 
-  class InterfaceComponent extends React.Component {
+  class SearchBar extends React.Component {
 
     constructor(){
       super();
+
     }
 
     keyUpHappened(evt){
       console.log(evt.keyCode);
       if (evt.keyCode === 13){
-        console.log(evt.target.value);
+        // console.log(evt.target.value);
         // GithubRepos.searchRepos(evt.target.value);
         this.setState({
           request:evt.target.value
@@ -24,40 +26,94 @@
       }
     }
 
+
+
     render(){
       var doSearch;
 
       if (this.state !== null){
-        doSearch = <GithubRepos query={this.state.request}/>;
+        doSearch = <GithubRepos query={this.state.request} page={1}/>;
+        console.log('maxPage')
+
       }
 
-      return (<div className="search-box">
-        <input className = "repo-input"
-        placeholder="Type a repo name and hit Enter"
-        onKeyUp={(evt) => {this.keyUpHappened(evt)}}/>
-        {doSearch}
+      return (<div>
+        <div className="search-box">
+          <input className = "repo-input"
+          placeholder="Type a repo name and hit Enter"
+          onKeyUp={(evt) => {this.keyUpHappened(evt)}}/>
+        </div>
+          {doSearch}
       </div>);
     }
 
   }
 
+
   class GithubRepos extends React.Component {
 
     constructor(){
       super();
-
     }
 
+    componentWillMount(){
+      console.log('component mounted',this.props);
+      // this.searchRepos(this.state.query);
+      // this.setState({
+      //   query:this.props.query,
+      //   pageNum:this.props.page
+      // })
+
+    }
     componentDidMount(){
-      console.log('component mounted');
-      this.searchRepos(this.props.query);
+
+      this.searchRepos(this.props.query,this.props.page);
+      this.setState({
+        query:this.props.query,
+        pageNum:this.props.page
+      })
     }
 
-    searchRepos(query){
+    nextButton(){
+      var page = this.state.pageNum;
+
+
+      if (page <= this.state.maxPage){
+        page ++;
+
+        this.setState({
+          pageNum:page
+        })
+
+        this.searchRepos(this.state.query,this.state.pageNum);
+
+      }
+
+    }
+
+    backButton(){
+      var page = this.state.pageNum;
+
+      if (page > 0) {
+
+        page --;
+
+        this.setState({
+          pageNum:page
+        })
+
+        this.searchRepos(this.state.query,this.state.pageNum);
+
+      }
+
+    }
+
+    searchRepos(query,page){
+
       $.ajax({
         type: 'GET',
         url: "https://api.github.com/search/repositories",
-        data:{q:query, page:pageNum},
+        data:{q:query, page:page},
         dataType: 'JSON',
         async:true,
         headers:{
@@ -67,10 +123,12 @@
       .done((repoData) => {
         //test
         console.log("recieved data ==>", repoData);
-        //
+
         this.setState({
-          apiResult:repoData
+          apiResult:repoData,
+          maxPage:Math.ceil(repoData.total_count/30)+1
         })
+
       })
       .fail(() => {
         console.log("API Call Failure");
@@ -80,28 +138,48 @@
     }
 
     render(){
+      // console.log("render sees page as",this.state.pageNum);
+      // console.log("render bar sees max-page as",this.state.maxPage);
       var searchResults;
+      var pageButtons;
 
-      if (this.state != null){
+      if (this.state !== null && this.state.apiResult !== undefined && this.state.pageNum !== undefined){
         searchResults =
-         <ul>
+         <ul className='results'>
           {this.state.apiResult.items.map((repo) => {return (
             <li key={repo.id}>
-              <div>{repo.name}</div>
-              <div>{repo.owner.login}</div>
+              <div className = 'repo-listing'>
+                <a href={repo.html_url}>{repo.name}</a>
+                <div className='repo-owner'>
+                  <a href={repo.owner.html_url}>{repo.owner.login}</a>
+                </div>
+              </div>
             </li>
           )})}
         </ul>;
+
+        if (this.state.maxPage > 1){
+          console.log("button bar sees page as",this.state.pageNum);
+          console.log("button bar sees max-page as",this.state.maxPage);
+          pageButtons =  <div className="pages">
+            <span className ="back"
+            onClick={()=>{this.backButton();}}>Back</span>
+            <span className="page-num">{this.state.pageNum}/{this.state.maxPage}</span>
+            <span className ="next"
+            onClick={()=>{this.nextButton();}}>Next</span>
+          </div>
+        }
+
       }
 
-      return <div className="repos-list"> {searchResults} </div>
+      return <div>{pageButtons}<div className="repos-list"> {searchResults}  </div></div>
     }
   }
 
   class AppComponent extends React.Component {
     render() {
       return (<div>React works!
-        <InterfaceComponent />
+        <SearchBar />
       </div>);
     }
   }
--- day-44-github-repo-search-react/public/script.js
+++ day-44-github-repo-search-react/public/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -10,28 +10,29 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 (function () {
 
+  //data should flow ONE WAY
+  //or seperate function to object outside components and use as argument
+
   var mountNode = document.querySelector('#react-root');
   var auth = btoa("4d892accbfd6b61cb0118fe2a5ce0baeebe5dad7");
 
   //placeholder
-  var pageNum = 1;
-  var query = "blue boomerang";
 
-  var InterfaceComponent = function (_React$Component) {
-    _inherits(InterfaceComponent, _React$Component);
+  var SearchBar = function (_React$Component) {
+    _inherits(SearchBar, _React$Component);
 
-    function InterfaceComponent() {
-      _classCallCheck(this, InterfaceComponent);
+    function SearchBar() {
+      _classCallCheck(this, SearchBar);
 
-      return _possibleConstructorReturn(this, (InterfaceComponent.__proto__ || Object.getPrototypeOf(InterfaceComponent)).call(this));
+      return _possibleConstructorReturn(this, (SearchBar.__proto__ || Object.getPrototypeOf(SearchBar)).call(this));
     }
 
-    _createClass(InterfaceComponent, [{
-      key: "keyUpHappened",
+    _createClass(SearchBar, [{
+      key: 'keyUpHappened',
       value: function keyUpHappened(evt) {
         console.log(evt.keyCode);
         if (evt.keyCode === 13) {
-          console.log(evt.target.value);
+          // console.log(evt.target.value);
           // GithubRepos.searchRepos(evt.target.value);
           this.setState({
             request: evt.target.value
@@ -39,30 +40,35 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         }
       }
     }, {
-      key: "render",
+      key: 'render',
       value: function render() {
         var _this2 = this;
 
         var doSearch;
 
         if (this.state !== null) {
-          doSearch = React.createElement(GithubRepos, { query: this.state.request });
+          doSearch = React.createElement(GithubRepos, { query: this.state.request, page: 1 });
+          console.log('maxPage');
         }
 
         return React.createElement(
-          "div",
-          { className: "search-box" },
-          React.createElement("input", { className: "repo-input",
-            placeholder: "Type a repo name and hit Enter",
-            onKeyUp: function onKeyUp(evt) {
-              _this2.keyUpHappened(evt);
-            } }),
+          'div',
+          null,
+          React.createElement(
+            'div',
+            { className: 'search-box' },
+            React.createElement('input', { className: 'repo-input',
+              placeholder: 'Type a repo name and hit Enter',
+              onKeyUp: function onKeyUp(evt) {
+                _this2.keyUpHappened(evt);
+              } })
+          ),
           doSearch
         );
       }
     }]);
 
-    return InterfaceComponent;
+    return SearchBar;
   }(React.Component);
 
   var GithubRepos = function (_React$Component2) {
@@ -75,20 +81,65 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
     }
 
     _createClass(GithubRepos, [{
-      key: "componentDidMount",
-      value: function componentDidMount() {
-        console.log('component mounted');
-        this.searchRepos(this.props.query);
+      key: 'componentWillMount',
+      value: function componentWillMount() {
+        console.log('component mounted', this.props);
+        // this.searchRepos(this.state.query);
+        // this.setState({
+        //   query:this.props.query,
+        //   pageNum:this.props.page
+        // })
       }
     }, {
-      key: "searchRepos",
-      value: function searchRepos(query) {
+      key: 'componentDidMount',
+      value: function componentDidMount() {
+
+        this.searchRepos(this.props.query, this.props.page);
+        this.setState({
+          query: this.props.query,
+          pageNum: this.props.page
+        });
+      }
+    }, {
+      key: 'nextButton',
+      value: function nextButton() {
+        var page = this.state.pageNum;
+
+        if (page <= this.state.maxPage) {
+          page++;
+
+          this.setState({
+            pageNum: page
+          });
+
+          this.searchRepos(this.state.query, this.state.pageNum);
+        }
+      }
+    }, {
+      key: 'backButton',
+      value: function backButton() {
+        var page = this.state.pageNum;
+
+        if (page > 0) {
+
+          page--;
+
+          this.setState({
+            pageNum: page
+          });
+
+          this.searchRepos(this.state.query, this.state.pageNum);
+        }
+      }
+    }, {
+      key: 'searchRepos',
+      value: function searchRepos(query, page) {
         var _this4 = this;
 
         $.ajax({
           type: 'GET',
           url: "https://api.github.com/search/repositories",
-          data: { q: query, page: pageNum },
+          data: { q: query, page: page },
           dataType: 'JSON',
           async: true,
           headers: {
@@ -97,9 +148,10 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         }).done(function (repoData) {
           //test
           console.log("recieved data ==>", repoData);
-          //
+
           _this4.setState({
-            apiResult: repoData
+            apiResult: repoData,
+            maxPage: Math.ceil(repoData.total_count / 30) + 1
           });
         }).fail(function () {
           console.log("API Call Failure");
@@ -107,39 +159,89 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         });
       }
     }, {
-      key: "render",
+      key: 'render',
       value: function render() {
-        var searchResults;
+        var _this5 = this;
 
-        if (this.state != null) {
+        // console.log("render sees page as",this.state.pageNum);
+        // console.log("render bar sees max-page as",this.state.maxPage);
+        var searchResults;
+        var pageButtons;
+
+        if (this.state !== null && this.state.apiResult !== undefined && this.state.pageNum !== undefined) {
           searchResults = React.createElement(
-            "ul",
-            null,
+            'ul',
+            { className: 'results' },
             this.state.apiResult.items.map(function (repo) {
               return React.createElement(
-                "li",
+                'li',
                 { key: repo.id },
                 React.createElement(
-                  "div",
-                  null,
-                  repo.name
-                ),
-                React.createElement(
-                  "div",
-                  null,
-                  repo.owner.login
+                  'div',
+                  { className: 'repo-listing' },
+                  React.createElement(
+                    'a',
+                    { href: repo.html_url },
+                    repo.name
+                  ),
+                  React.createElement(
+                    'div',
+                    { className: 'repo-owner' },
+                    React.createElement(
+                      'a',
+                      { href: repo.owner.html_url },
+                      repo.owner.login
+                    )
+                  )
                 )
               );
             })
           );
+
+          if (this.state.maxPage > 1) {
+            console.log("button bar sees page as", this.state.pageNum);
+            console.log("button bar sees max-page as", this.state.maxPage);
+            pageButtons = React.createElement(
+              'div',
+              { className: 'pages' },
+              React.createElement(
+                'span',
+                { className: 'back',
+                  onClick: function onClick() {
+                    _this5.backButton();
+                  } },
+                'Back'
+              ),
+              React.createElement(
+                'span',
+                { className: 'page-num' },
+                this.state.pageNum,
+                '/',
+                this.state.maxPage
+              ),
+              React.createElement(
+                'span',
+                { className: 'next',
+                  onClick: function onClick() {
+                    _this5.nextButton();
+                  } },
+                'Next'
+              )
+            );
+          }
         }
 
         return React.createElement(
-          "div",
-          { className: "repos-list" },
-          " ",
-          searchResults,
-          " "
+          'div',
+          null,
+          pageButtons,
+          React.createElement(
+            'div',
+            { className: 'repos-list' },
+            ' ',
+            searchResults,
+            '  '
+          )
         );
       }
     }]);
@@ -157,13 +259,13 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
     }
 
     _createClass(AppComponent, [{
-      key: "render",
+      key: 'render',
       value: function render() {
         return React.createElement(
-          "div",
+          'div',
           null,
-          "React works!",
-          React.createElement(InterfaceComponent, null)
+          'React works!',
+          React.createElement(SearchBar, null)
         );
       }
     }]);
